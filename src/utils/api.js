c0fd90b4b565e1f9c8eb1e3d420c594f42238f3d
pratyushkumarbarik <pratyushkumar480@gmail.com>
2025-09-08 12:54:40 +0530

    frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Use your actual backend API domain with https://
-const API_BASE_URL = 'https://vercel-backend-lostfound.onrender.com'; // Or your Render backend URL
+const API_BASE_URL = 'https://backend-render-l8re.onrender.com'; // Or your Render backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
